93fb642f0df5318d09a787eac3d7399b3da9b20b
Ryan Mark <rymark907@gmail.com>
2021-09-30 13:13:55 -0700

    worked through tests 1-7

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -33,25 +33,103 @@ test('renders ONE error message if user enters less then 5 characters into first
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+
+    const Error1 = screen.getByText(/error: firstname must have at least 5 characters./i);
+    const Error2 = screen.getByText(/Error: lastName is a required field./i);
+    const Error3 = screen.getByText(/Error: email must be a valid email address./i);
+    //Assert
+    expect(Error1).toBeInTheDocument();
+    expect(Error2).toBeInTheDocument();
+    expect(Error3).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const firstNameInput = screen.getByPlaceholderText(/edd/i);
+    userEvent.type(firstNameInput, "Robert");
+
+    const lastNameInput = screen.getByPlaceholderText(/burke/i);
+    userEvent.type(lastNameInput, "Williamson");
+
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+     
+    const emailError = screen.getByText(/Error: email must be a valid email address./i);
+    //Assert
+    expect(emailError).toBeInTheDocument();
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    userEvent.type(emailInput, "something");
+
+    const emailError = screen.getByText(/Error: email must be a valid email address./i);
+    //Assert
+    expect(emailError).toBeInTheDocument();
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const firstNameInput = screen.getByPlaceholderText(/edd/i);
+    userEvent.type(firstNameInput, "Robert");
+
+    const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    userEvent.type(emailInput, "something@something.com");
+
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+
+    const lastNameError = screen.getByText(/Error: lastName is a required field./i);
+    //Assert
+    expect(lastNameError).toBeInTheDocument();
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const firstNameInput = screen.getByPlaceholderText(/edd/i);
+    userEvent.type(firstNameInput, "Robert");
+
+    const lastNameInput = screen.getByPlaceholderText(/burke/i);
+    userEvent.type(lastNameInput, "Williamson");
+
+    const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    userEvent.type(emailInput, "something@something.com");
+
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+
+    const messageText = screen.getByText(/message:/i);
+    //Assert
+    expect(messageText).toBeFalsy();
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    // const firstNameInput = screen.getByPlaceholderText(/edd/i);
+    // userEvent.type(firstNameInput, "Robert");
+
+    // const lastNameInput = screen.getByPlaceholderText(/burke/i);
+    // userEvent.type(lastNameInput, "Williamson");
+
+    // const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    // userEvent.type(emailInput, "something@something.com");
+
+    // const submitButton = screen.getByRole("button");
+    // userEvent.click(submitButton);
 });
